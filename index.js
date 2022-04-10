5364bfa6735f773d4a1b4f0359b23d414892ecb4
v_yaoxzhang <v_yaoxzhang@tencent.com>
2022-04-10 15:35:44 +0800

    feat: get user setting options

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-module.exports = function (ctx) {
+const loaderUtils = require('loader-utils')
+
+/**
+ * @param {string} content 文件的文本内容
+ */
+module.exports = function (content) {
   // 启用缓存
   if (this.cacheable) {
     this.cacheable()
   }
+  // 获取打印 用户项目中的配置对象 use.options
+  const options = loaderUtils.getOptions(this) || {}
+  console.log('🚀 → loader-options: ', options)
+
   const useStrictPrefix = "\'use strict\';\n\n";
-  return useStrictPrefix + ctx;
+  return useStrictPrefix + content;
 }
