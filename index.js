c6bceb2bbf2cf7f08afb222a52eeb4dee52d4d73
v_yaoxzhang <v_yaoxzhang@tencent.com>
2022-04-10 15:29:52 +0800

    feat: enable the cache

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = function (ctx) {
+  // 启用缓存
+  if (this.cacheable) {
+    this.cacheable()
+  }
   const useStrictPrefix = "\'use strict\';\n\n";
   return useStrictPrefix + ctx;
 }
